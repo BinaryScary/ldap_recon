7789bc4d9c8a05cc1957f86e8b48a198c76eba7c
Samwong156 <sam_wong156@hotmail.com>
2022-03-09 13:56:49 -0500

    added dynamic queries, ldap time -> datetime string

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use ldap3::{Ldap, LdapConnAsync, Scope, SearchEntry, result::Result};
 use ansi_term::Colour;
 use serde::{Serialize, Deserialize};
-use std::fs;
+use std::{fs,time::SystemTime};
+use chrono::{DateTime, Duration, Utc};
 use clap::Parser;
+use itertools::Itertools;
+
+// TODO: rootdse, appropriate attributes for certs
 
 // query struct
 // ldap query char escapes: https://tools.ietf.org/search/rfc2254#page-5
@@ -30,9 +34,29 @@ struct Args {
     #[clap(short, long)]
     pass: String,
 
-    // config file
+    /// config file (Helper Strings: [TARGETDN] [-30DAYS]) (Queries cannot contain spaces) (use * for all attributes)
     #[clap(short, long, default_value = "vulnerable.json")]
     config: String,
+}
+
+fn adtime_to_string(adtime: u128) -> Result<String>{
+    if adtime == 0 {
+        return Ok(String::from("0000-00-00 00:00:00"));
+    }
+
+    // 100 interval nanoseconds to seconds
+    let seconds = (adtime  / 1000000000) * 100;
+    // Windows NT time format 
+    let ad_epoch : SystemTime = DateTime::parse_from_rfc2822("1 Jan 1601 00:00:00 +0000").unwrap().into();
+    // add seconds to windows epoch
+    let duration = ad_epoch + Duration::seconds(i64::try_from(seconds).unwrap()).to_std().unwrap();
+
+    // convert duration to timestamp
+    let datetime = DateTime::<Utc>::from(duration);
+    let timestamp_str = datetime.format("%Y-%m-%d %H:%M:%S").to_string();
+
+    return Ok(timestamp_str);
+    // let duration = Duration::seconds(i64::try_from(seconds).unwrap());
 }
 
 // to_string helper, since you cannot impl display trait outside crate
@@ -47,11 +71,21 @@ fn resultentries_to_string(rs: Vec<ldap3::ResultEntry>) -> Result<String>{
         // output.push_str(&search_entry.dn);
         output.push_str("\n");
 
-        // add attributes and values to string
-        for (name,attribute) in search_entry.attrs{
+        // add attributes and values to string (sorted)
+        for name in search_entry.attrs.keys().sorted(){
+            // add attribute name
             output.push_str(&format!("{}: ",name));
-            for value in attribute{
-                output.push_str(&value);
+            match name.as_str(){
+                "pwdLastSet" | "LastPwdSet" | "accountExpires" | "LastLogon" | "LastLogonTimestamp" =>{
+                    for value in &search_entry.attrs[name]{
+                        output.push_str(&adtime_to_string(value.parse::<u128>().unwrap())?);
+                    }
+                },
+                _ =>{
+                    for value in &search_entry.attrs[name]{
+                        output.push_str(&value);
+                    }
+                } 
             }
             output.push_str("\n");
         }
@@ -91,7 +125,7 @@ async fn main() -> Result<()> {
 
     // read queries from json file
     let config = fs::File::open(args.config).expect("config file should be provided");
-    let queries: Vec<Query> = serde_json::from_reader(config).expect("config.json should be proper JSON");
+    let queries: Vec<Query> = serde_json::from_reader(config).expect("config should be proper JSON");
 
     // bind to ldap server
     let (conn, mut ldap) = LdapConnAsync::new(&format!("ldap://{}:389",args.host)).await?;
@@ -100,13 +134,33 @@ async fn main() -> Result<()> {
 
     // get base dn
     let dn = get_dn(&mut ldap).await?;
+
+    // ldap measures time as 100 nansecond intervals since January 1, 1601 UTC 
+    let ad_epoch : SystemTime = DateTime::parse_from_rfc2822("1 Jan 1601 00:00:00 +0000").unwrap().into();
+    let ad_ct = SystemTime::now().duration_since(ad_epoch).unwrap().as_nanos() / 100;
+    let ad_year = u128::try_from(Duration::days(365).num_nanoseconds().unwrap()).unwrap() / 100;
+    let ad_month = u128::try_from(Duration::days(30).num_nanoseconds().unwrap()).unwrap() / 100;
+    let ad_week = u128::try_from(Duration::days(7).num_nanoseconds().unwrap()).unwrap() / 100;
     
     // TODO: make asynchronous
-    // TODO: custom queries, i.e: nested domain admins, computers with password last set
     for mut query in queries{
-        println!("{}:", Colour::Purple.underline().bold().paint(&query.name));
-        // println!("{}:", query.name);
+        // if base_dn contains a distinguished name but not a comma
+        if query.base_dn != "" && query.base_dn.chars().last().unwrap() != ',' {
+            query.base_dn.push_str(",");
+        }
+        // add dn to query base_dn search
         query.base_dn.push_str(&dn);
+        // replaces strings in dynamic queries
+        // TODO: could make this more dynamic, IE check for integer inbetween '[-' and 'DAYS]' or RFC2822 time
+        query.query = str::replace(&query.query, "[TARGETDN]",&dn);
+        query.query = str::replace(&query.query, "[-1YEAR]",&(ad_ct - ad_year).to_string());
+        query.query = str::replace(&query.query, "[-30DAYS]",&(ad_ct - ad_month).to_string());
+        query.query = str::replace(&query.query, "[-7DAYS]",&(ad_ct - ad_week).to_string());
+
+        println!("{}:", Colour::Purple.underline().bold().paint(&query.name));
+        println!("{}", Colour::Purple.paint(format!("Base: {}",&query.base_dn)));
+        println!("{}", Colour::Purple.paint(format!("Query: {}",&query.query)));
+        // println!("{}:", query.name);
         let rs = run_query(&mut ldap, &query).await?;
         println!("{}", rs);
     }
